245517a90cf92f8a3a4819f1429adb2b1df20f47
NayongLee <iny003@naver.com>
2016-10-17 14:04:37 +0900

    수정

--- AndroidStudy/Quickcoding01/src/main/java/com/example/quickcoing01/MainActivity.java
+++ AndroidStudy/Quickcoding01/src/main/java/com/example/quickcoing01/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
